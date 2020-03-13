376e3e0e8c2b127eb42d20504da4452686e3b1a5
Shishir Adhikari <sadhik9@uic.edu>
2020-03-13 01:17:51 -0500

    Changed the implementation of custom conv to make it comptabile with CRF. Need to handle multiple zero paddings.

--- code/batch_custom_Conv.py
+++ code/batch_custom_Conv.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn as nn
 import math
 import torch.optim as optim
-torch.manual_seed(123)
-torch.cuda.manual_seed(123)
+# torch.manual_seed(123)
+# torch.cuda.manual_seed(123)
 
 
 class builtinConv(nn.Module):
@@ -25,40 +25,41 @@ class myConv(nn.Module):
     myConvolution layer.
     """
 
-    def __init__(self, in_chan, out_chan, k_size, batch_size, stride, pad, b):
+    def __init__(self, in_chan, out_chan, k_size, stride, pad, b=False):
         super(myConv, self).__init__()
         self.in_channels = in_chan
         self.out_channels = out_chan
         self.kernel_size = k_size
+        self.stride = stride
         self.str_row = stride[0]
         self.str_col = stride[1]
         self.padding = pad
         self.bias = b
-        self.filter_weights = nn.Parameter(torch.randn((1, 1, self.kernel_size, self.kernel_size), dtype=torch.float64))
+        self.weight = nn.Parameter(torch.randn((1, 1, self.kernel_size, self.kernel_size), dtype=torch.float64))
         #self.filter_weights = nn.Parameter(torch.tensor([1, 0, 1, 0, 1, 0, 1, 0, 1], dtype=torch.float64).view(1, 1, 3, 3))
-        self.batch_size = batch_size
 
     def init_params(self, X):
          pass
 
     def forward(self, X):
-        if self.padding == True:
+        batch_size = X.shape[0]
+        if self.padding > 0:
             m = nn.ZeroPad2d(1)
             X = m(X)
         X_rows = X.shape[2]
         X_cols = X.shape[3]
-        K_rows = self.filter_weights.shape[2]
-        K_cols = self.filter_weights.shape[3]
+        K_rows = self.weight.shape[2]
+        K_cols = self.weight.shape[3]
         result_xdim = math.floor(((X_rows - (K_rows-1)-1) / self.str_row) + 1)
         result_ydim = math.floor(((X_cols - (K_cols-1)-1) / self.str_col) + 1)
-        result = torch.zeros([self.batch_size, self.out_channels, result_xdim, result_ydim], dtype=torch.float64)
+        result = torch.zeros([batch_size, self.out_channels, result_xdim, result_ydim], dtype=torch.float64)
         for i in range(0, result_xdim):
             for j in range(0, result_ydim):
                 #calculates a single activation for all samples in the batch by using torch.narrow()
-                batch_conv_units = torch.sum(torch.sum(torch.mul(self.filter_weights, torch.narrow(torch.narrow(X, 2, i*self.str_row, K_rows), 3, j*self.str_col, K_cols)), -1),-1)
-                print(batch_conv_units)
+                batch_conv_units = torch.sum(torch.sum(torch.mul(self.weight, torch.narrow(torch.narrow(X, 2, i*self.str_row, K_rows), 3, j*self.str_col, K_cols)), -1),-1)
+                # print(batch_conv_units)
                 result[:, :, i, j] = batch_conv_units
-        print(result)
+        # print(result)
         return result
 
 
@@ -80,7 +81,7 @@ if __name__ == '__main__':
 
 
     # instantiate from  my_conv and builtin_conv to compare grads
-    my_conv = myConv(in_channels, out_channels, kernel_size, batch_size, stride, padding, bias)
+    my_conv = myConv(in_channels, out_channels, kernel_size, stride, padding, bias)
     builtin_conv = builtinConv(in_channels, out_channels, kernel_size, stride2, pad, bias)
 
     # define loss function and optimizer
--- code/test_myConv.py
+++ code/test_myConv.py
@@ -1,8 +1,8 @@
 from batch_custom_Conv import myConv
 from read_data import read_tensor_padding
 print("Loading Dataset...")
-trainSet = read_tensor_padding('data/train.txt')
-testSet = read_tensor_padding('data/train.txt')
+trainSet = read_tensor_padding('../data/train.txt')
+testSet = read_tensor_padding('../data/train.txt')
 print("Dataset Loaded ...")
 words = trainSet[:][0]
 labels = trainSet[:][1]
@@ -10,11 +10,11 @@ labels = trainSet[:][1]
 trainConv = trainSet[:][0]
 batch_size, max_chars, dimX = trainConv.shape
 all_images = trainConv.reshape(-1,1,16,8)
-conv_layer = myConv(in_chan=1, out_chan=1, k_size=5, batch_size=2, stride=[1, 1], pad=True, b=False)
+conv_layer = myConv(in_chan=1, out_chan=1, k_size=3, stride=[1, 1], pad=True, b=False)
 print(all_images[:2].shape)
-out = conv_layer(all_images[:2])
+out = conv_layer(all_images)
 print(out.shape)
 #out = out.view(-1, 64)
 out = out.view(-1, out.shape[2]*out.shape[3])
 #out = out.reshape(batch_size, max_chars, -1)
-print(out)
+print(out.shape)
